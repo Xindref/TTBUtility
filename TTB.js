cc0bebd43572405baec6c24ecb2227d1268d44b3
Dako <dakotahmccrary@gmail.com>
2023-03-15 05:20:56 -0500

    Changed barcode generation method and added text lists for green and red totes

--- TTB.js
+++ TTB.js
@@ -14,11 +14,33 @@ function randBarcode() {
     return newBarcode;
 }
 
-function generateBarcodes(count) {
+function generateBarcode(value, color) {
+    let img = new Image(200, 100);
+    img.classList.add(`barcode-${color}`);
+    JsBarcode(img, value, { format: "CODE128" });
+    document.getElementById("selectedTextBarcode").appendChild(img);
+}
+
+function generateListItem(count, color) {
     for (let i = 0; i < count; i++) {
-        let img = new Image(200, 100);
-        img.classList.add("barcode");
-        JsBarcode(img, randBarcode(), { format: "CODE128" });
-        document.getElementById("barcodeContainer").appendChild(img);
+        let p = document.createElement('p');
+        p.innerText = randBarcode();
+        document.querySelector(`#${color}ToteList`).append(p);
+        p.addEventListener('click', function (e) {
+            document.querySelector('#selectedTextBarcode').innerHTML = '';
+            generateBarcode(e.target.innerText, `${color}`);
+        })
     }
 }
+
+document.querySelector('#ineligibleCount').addEventListener('click', function () {
+    amount = document.querySelector('#ineligibleCount').innerText;
+    document.querySelector('#redToteList').innerHTML = '';
+    generateListItem(amount, 'red');
+});
+
+document.querySelector('#eligibleCount').addEventListener('click', function () {
+    amount = document.querySelector('#eligibleCount').innerText;
+    document.querySelector('#greenToteList').innerHTML = '';
+    generateListItem(amount, 'green');
+});
